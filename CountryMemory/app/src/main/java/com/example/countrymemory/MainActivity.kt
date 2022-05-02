7f9f1962b0b5b4c4fee212706080c1b01a11e982
khenissimehdi <khenissimehdi0123@gmail.com>
2022-05-02 21:28:24 +0200

    screen splitted

--- CountryMemory/app/src/main/java/com/example/countrymemory/MainActivity.kt
+++ CountryMemory/app/src/main/java/com/example/countrymemory/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.countrymemory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.countrymemory.ui.theme.CountryMemoryTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +27,9 @@ class MainActivity : ComponentActivity() {
             CountryMemoryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Splash()
                 }
+
             }
         }
     }
@@ -36,5 +45,37 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     CountryMemoryTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun Splash() {
+
+    Row(modifier =Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Blue)
+                .weight(1f)
+                .padding(8.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column() {
+                Text(text = "Example", fontSize = 44.sp)
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Red)
+                .weight(1f)
+                .padding(8.dp),
+            contentAlignment = Alignment.Center
+        ){
+            Column {
+
+                Text(text = "Example", textAlign = TextAlign.End, color = DarkGray, fontSize = 12.sp)
+            }
+        }
     }
 }
